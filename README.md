f812e7110dd07c539f2286518226eca9bc3e9bbe
JeremyLleva <llevajeremy@yahoo.com>
2020-08-03 23:12:13 -0700

    Fresh commit reset project

--- README.md
+++
@@ -1,2 +0,0 @@
-# Mobile-Shopping-Applications
-A mobile shopping app created using react native
---
+++ constants/Colors.js
@@ -0,0 +1,6 @@
+/** @format */
+
+export default {
+    primary: '#C2185B',
+    accent: '#FFC107',
+}
