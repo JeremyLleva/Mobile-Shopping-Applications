819097471c1a354591803ee30a3fce609fb298f5
JeremyLleva <llevajeremy@yahoo.com>
2020-08-04 11:01:42 -0700

    Working Product list page and Product details page

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 /** @format */
 
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { AppLoading } from 'expo'
+import * as Font from 'expo-font'
 
 import productsReducer from './store/reducers/products'
 import ShopNavigator from './navigation/ShopNavigator'
@@ -15,7 +17,25 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer)
 
+const fetchFonts = () => {
+    return Font.loadAsync({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    })
+}
+
 export default function App() {
+    const [fontLoaded, setFontLoaded] = useState(false)
+    if (!fontLoaded) {
+        return (
+            <AppLoading
+                startAsync={fetchFonts}
+                onFinish={() => {
+                    setFontLoaded(true)
+                }}
+            />
+        )
+    }
     return (
         <Provider store={store}>
             <ShopNavigator />
