bb4ea42cbc1a3fe47c96ce33b932caff8e2772ef
JeremyLleva <llevajeremy@yahoo.com>
2020-08-04 17:24:25 -0700

    delete functionality working

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,13 +1,31 @@
 /** @format */
-
+import React from 'react'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
+import { createDrawerNavigator } from 'react-navigation-drawer'
 import { Platform } from 'react-native'
-
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen'
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen'
 import CartScreen from '../screens/shop/CartScreen'
+import OrdersScreen from '../screens/shop/OrdersScreen'
+import UserProductsScreen from '../screens/user/UserProductsScreen'
+import EditProductsScreen from '../screens/user/EditProductsScreen'
+
 import Colors from '../constants/Colors'
+import { Ionicons } from '@expo/vector-icons'
+
+const defaultNavOptions = {
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+    },
+    headerTitleStyle: {
+        fontFamily: 'open-sans-bold',
+    },
+    headerBackTitleStyle: {
+        fontFamily: 'open-sans',
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+}
 
 const ProductsNavigator = createStackNavigator(
     {
@@ -16,21 +34,67 @@ const ProductsNavigator = createStackNavigator(
         Cart: CartScreen,
     },
     {
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor:
-                    Platform.OS === 'android' ? Colors.primary : '',
-            },
-            headerTitleStyle: {
-                fontFamily: 'open-sans-bold',
-            },
-            headerBackTitleStyle: {
-                fontFamily: 'open-sans',
-            },
-            headerTintColor:
-                Platform.OS === 'android' ? 'white' : Colors.primary,
+        navigationOptions: {
+            drawerIcon: (drawerConfig) => (
+                <Ionicons
+                    name='md-list'
+                    size={23}
+                    color={drawerConfig.intColor}
+                />
+            ),
+        },
+        defaultNavigationOptions: defaultNavOptions,
+    }
+)
+
+const AdminNavigator = createStackNavigator(
+    {
+        UserProducts: UserProductsScreen,
+        EditProduct: EditProductsScreen,
+    },
+    {
+        navigationOptions: {
+            drawerIcon: (drawerConfig) => (
+                <Ionicons
+                    name='md-create'
+                    size={23}
+                    color={drawerConfig.intColor}
+                />
+            ),
+        },
+        defaultNavigationOptions: defaultNavOptions,
+    }
+)
+
+const OrdersNavigator = createStackNavigator(
+    {
+        Orders: OrdersScreen,
+    },
+    {
+        navigationOptions: {
+            drawerIcon: (drawerConfig) => (
+                <Ionicons
+                    name='md-cart'
+                    size={23}
+                    color={drawerConfig.intColor}
+                />
+            ),
+        },
+        defaultNavigationOptions: defaultNavOptions,
+    }
+)
+
+const ShopNavigator = createDrawerNavigator(
+    {
+        Products: ProductsNavigator,
+        Orders: OrdersNavigator,
+        Admin: AdminNavigator,
+    },
+    {
+        contentOptions: {
+            activeTintColor: Colors.primary,
         },
     }
 )
 
-export default createAppContainer(ProductsNavigator)
+export default createAppContainer(ShopNavigator)
--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,10 +1,14 @@
 /** @format */
 
 import React from 'react'
-import { View, Text, FlatList, StyleSheet, Button } from 'react-native'
-import { useSelector } from 'react-redux'
+import { View, Text, FlatList, Button, StyleSheet } from 'react-native'
+import { useSelector, useDispatch } from 'react-redux'
+
 import Colors from '../../constants/Colors'
 import CartItem from '../../components/shop/CartItem'
+import Card from '../../components/UI/Card'
+import * as cartActions from '../../store/actions/cart'
+import * as ordersActions from '../../store/actions/orders'
 
 const CartScreen = (props) => {
     const cartTotalAmount = useSelector((state) => state.cart.totalAmount)
@@ -19,23 +23,32 @@ const CartScreen = (props) => {
                 sum: state.cart.items[key].sum,
             })
         }
-        return transformedCartItems
+        return transformedCartItems.sort((a, b) =>
+            a.productId > b.productId ? 1 : -1
+        )
     })
+    const dispatch = useDispatch()
+
     return (
         <View style={styles.screen}>
-            <View style={styles.summary}>
+            <Card style={styles.summary}>
                 <Text style={styles.summaryText}>
                     Total:{' '}
                     <Text style={styles.amount}>
-                        ${cartTotalAmount.toFixed(2)}
+                        ${Math.round(cartTotalAmount.toFixed(2) * 100) / 100}
                     </Text>
                 </Text>
                 <Button
                     color={Colors.accent}
                     title='Order Now'
                     disabled={cartItems.length === 0}
+                    onPress={() => {
+                        dispatch(
+                            ordersActions.addOrder(cartItems, cartTotalAmount)
+                        )
+                    }}
                 />
-            </View>
+            </Card>
             <FlatList
                 data={cartItems}
                 keyExtractor={(item) => item.productId}
@@ -44,12 +57,23 @@ const CartScreen = (props) => {
                         quantity={itemData.item.quantity}
                         title={itemData.item.productTitle}
                         amount={itemData.item.sum}
-                        onRemove={() => {}}
+                        deletable
+                        onRemove={() => {
+                            dispatch(
+                                cartActions.removeFromCart(
+                                    itemData.item.productId
+                                )
+                            )
+                        }}
                     />
                 )}
             />
         </View>
     )
+}
+
+CartScreen.navigationOptions = {
+    headerTitle: 'Your Cart',
 }
 
 const styles = StyleSheet.create({
@@ -62,13 +86,6 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         marginBottom: 20,
         padding: 10,
-        shadowColor: 'black',
-        shadowOpacity: 0.26,
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 8,
-        elevation: 5,
-        borderRadius: 10,
-        backgroundColor: 'white',
     },
     summaryText: {
         fontFamily: 'open-sans-bold',
@@ -78,4 +95,5 @@ const styles = StyleSheet.create({
         color: Colors.primary,
     },
 })
+
 export default CartScreen
